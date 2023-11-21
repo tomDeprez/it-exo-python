1103d8cad7e8e77b43ce68d0ddf48542694f3275
tom <tom_deprez@hotmail.fr>
2023-11-21 16:45:50 +0100

    Créer l'exo 5

---
+++ exo5.py
@@ -0,0 +1,5 @@
+# Exercice 5 : Vérification de Palindrome
+# Créer un programme qui vérifie si un mot est un palindrome (se lit de la même manière dans les deux sens).
+# Exemple :
+# Entrée : "radar"
+# Sortie : True
