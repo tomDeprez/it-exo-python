5c0f7191d801e116f3114b82e3ff47b9e1074625
tom <tom_deprez@hotmail.fr>
2023-11-21 10:27:40 +0100

    modification de l'exo 2 en python

--- exo2.py
+++ exo2.py
@@ -1,11 +1,26 @@
 array = [10, 22, 64, 20, 1]
 total = 0
 
+for i in range(len(array)):
+    print(array[i])
+
+for i in array:
+    print(i)
+
+
+for tom in array:
+    total = total + tom
+print(total)
+
+total = 0
+
+for tom in range(len(array)) :
+    total = total + array[tom]
+print(total)
+
+
 # Exercice 2 : Somme des Éléments d'une Liste
 # Créer un algorithme qui calcule la somme des éléments d'une liste.
 # Exemple :
 # Entrée : [1, 2, 3, 4]
 # Sortie : 10
-
-for i in array:
-    print(i)
