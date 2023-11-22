19cf239d5ad851128d40d91f7061940670e88b3b
tom <tom_deprez@hotmail.fr>
2023-11-22 11:41:08 +0100

    commit de l'exo 5

--- exo5.py
+++ exo5.py
@@ -3,3 +3,34 @@
 # Exemple :
 # Entrée : "radar"
 # Sortie : True
+
+# r != r
+# a != a
+def printResult(entree, result) :
+    if result == entree:
+        return True
+    else :
+        return False
+entree = "bonjour"
+# bonjour
+# ruojnob != bonjour
+array = list(entree)
+
+count = len(array)
+result = ""
+for i in range(count) : 
+    result = result + array[count - (i + 1)]
+
+print(printResult(entree, result))
+
+
+result = ""
+for i in array:
+    result = i + result
+
+test = printResult(entree, result)
+
+if printResult(entree, result) :
+    print(printResult(entree, result))
+else :
+    print(not printResult(entree, result))
